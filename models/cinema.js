3695f8ee5e07da641e6035160bd54393e442db3d
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-11-18 14:46:59 +0000

    first pass

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,17 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function(films) {
+//   const result = films.filter((film) => {
+//     return film;
+//   });
+//   return result;
+// }
+const result = [];
+films.forEach((film) => {
+  result.push(film.title);
+});
+return result;
+}
+
 module.exports = Cinema;
